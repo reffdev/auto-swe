72960206ce68b7ed0da7eb070913269d159c5b06
auto-swe <auto-swe@localhost>
2026-03-26 18:44:44 -0500

    LLM Log DB Layer (#7)

--- jest.setup.ts
+++ jest.setup.ts
@@ -3,3 +3,7 @@ import '@testing-library/jest-dom'
 // Polyfill for TextEncoder/TextDecoder which are required by react-router
 global.TextEncoder = require('util').TextEncoder
 global.TextDecoder = require('util').TextDecoder
+
+// Polyfill for ReadableStream and TransformStream which are required by @langchain/core
+global.ReadableStream = require('stream/web').ReadableStream
+global.TransformStream = require('stream/web').TransformStream
